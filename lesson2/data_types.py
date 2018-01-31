ad682c8041dfa819b34cb4e2f21138d9f38a9d15
Ian Blumenfeld <ian@Ians-MacBook-Air-2.local>
2018-01-30 17:34:36 -0800

    Initial commit for data_types

---
+++ lesson2/data_types.py
@@ -0,0 +1,30 @@
+"""This lesson will talk about the different basic data types that
+exist in python.  We'll start with a few that you are probably mostly
+familiar with and then we'll move on to ones that are a little bit
+more esoteric, but very useful.
+
+Before we start, we should explain what a "type" is.  A "type" tells
+you what kind of data you have.  For example whole numbers like 0, 1,
+-2, 3456, etc have the type "integer."  Numbers that have a decimal
+point like 2.5, 3.1415925635, and even 1.0 have a different type
+called "float."  Now, the name "float" doesn't make a ton of sense
+right now.  Just trust me, there is a good reason they're called that.
+(It has to do with how the computer manipulates them.)
+
+Integers and floats are the basic numeric types.  They're about as
+simple as things can get.  You can add, subtract, multiply, and divide
+them.  Weird things will happen if you try to divide by 0, just like
+in real math.  Try not to do that.  More on this later.
+
+In your python interpreter try playing around with integers.  You
+access the interpreter by just typing "python" in your terminal
+
+"""
+### Exercise: Type these in the interpreter.  Does anything weird happen?
+
+# 1 + 1
+# 4 * 100
+# 10 / 5
+# 10 / 3
+# 10 / 0
+
